a4c93804423bc6ac9bc172dd4aa671948274177f
Avinache <avinache@gmail.com>
2025-06-09 22:22:22 +0530

    Add files via upload

--- PowerPulse.py
+++ PowerPulse.py
@@ -11,6 +11,23 @@ import warnings
 warnings.filterwarnings('ignore')
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_percentage_error
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+from sklearn.model_selection import GridSearchCV
+
+# Initialize standard scalar and minmax scalar
+mm = MinMaxScaler()
+ss = StandardScaler()
+
 #Define the file path
 file_path = 'household_power_consumption.txt'
 # Read the file into a DataFrame and combine data & time in one column
@@ -130,9 +147,6 @@ else:
 v, _ = stats.boxcox(data['Global_reactive_power'])
 data['Global_reactive_power'] = v
 
-# Initialize standard scalar and minmax scalar
-mm = MinMaxScaler()
-ss = StandardScaler()
 
 # Standardize the value of reactive power column
 data['Global_reactive_power'] = mm.fit_transform(data[['Global_reactive_power']])
@@ -148,12 +162,6 @@ sns.kdeplot(data['Voltage'])
 val = data.drop(['DateTime', "Global_active_power"], axis = 1)
 tar = data['Global_active_power']
 
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_percentage_error
-from scipy import stats
-from sklearn.model_selection import GridSearchCV
-
 
 # Train and test split 
 traindata, testdata, trainlab, testlab = train_test_split( val, tar, test_size= 0.20, random_state= 46 )
@@ -162,7 +170,7 @@ traindata, testdata, trainlab, testlab = train_test_split( val, tar, test_size= 
 model = LinearRegression()
 model.fit(traindata, trainlab)
 
-# train prediction 
+# prediction 
 tr_pred = model.predict(traindata)
 ts_pred = model.predict(testdata)
 
@@ -194,3 +202,96 @@ root_mean_squared_error( trainlab, tr_pred )
 
 mean_squared_error( testlab, ts_pred )
 root_mean_squared_error( testlab, ts_pred )
+
+
+# Split val and target 
+val = data.drop(['DateTime', "Global_active_power"], axis = 1)
+tar = data['Global_active_power']
+
+# Train and test split 
+trdata, tsdata, trlab, tslab = train_test_split( val, tar, test_size= 0.20, random_state= 46 )
+
+# Model Selection
+model = KNeighborsRegressor(n_neighbors=5)
+model.fit(trdata, trlab)
+
+# prediction
+tr_pred = model.predict(trdata)
+ts_pred = model.predict(tsdata)
+
+#Performance check
+mean_squared_error( trlab, tr_pred )
+root_mean_squared_error( trlab, tr_pred )
+
+mean_squared_error( tslab, ts_pred )
+root_mean_squared_error( tslab, ts_pred )
+
+
+#Tuning
+para = {
+    "n_neighbors": list(range(3, 5, 2)),
+    "metric": ["minkowski"],
+    "leaf_size": [30,10,20],
+    "p": [1,2]
+}
+
+# Built Model
+knn = KNeighborsRegressor()
+gsv = GridSearchCV(param_grid= para, cv = 5, estimator= knn)
+gsv.fit(trdata, trlab)
+
+# Prediction
+tr_pred = gsv.best_estimator_.predict(trdata)
+ts_pred = gsv.best_estimator_.predict(tsdata)
+
+# Performance Check
+mean_squared_error( trlab, tr_pred )
+root_mean_squared_error( trlab, tr_pred )
+
+mean_squared_error( tslab, ts_pred )
+root_mean_squared_error( tslab, ts_pred )
+
+
+# Split val and target 
+val = data.drop(['DateTime', "Global_active_power"], axis = 1)
+tar = data['Global_active_power']
+
+
+#Decision tree feature selection
+fs = RandomForestRegressor(n_estimators= 200, random_state= 66)
+fs.fit(val, tar)
+
+fs.feature_importances_
+
+pd.DataFrame({
+    "Columns": val.columns,
+    "Score": fs.feature_importances_ * 100
+}).sort_values("Score", ascending = False)
+
+
+# Feature selection 
+selected_col = pd.DataFrame({
+    "Columns": val.columns,
+    "Score": fs.feature_importances_ * 100
+}).sort_values("Score", ascending = False).head(4)['Columns'].to_list()
+
+# Split val and target based on feature selection
+val = val[selected_col]
+tar = data['Global_active_power']
+trdata, tsdata, trlab, tslab = train_test_split(val, tar, test_size= 0.20, random_state= 60)
+
+# Built Model
+dt_model = DecisionTreeRegressor(random_state= 60)
+dt_model.fit(trdata, trlab)
+
+# Prediction
+tr_pred = dt_model.predict(trdata)
+ts_pred = dt_model.predict(tsdata)
+
+# Performance Check
+mean_squared_error( trlab, tr_pred )
+root_mean_squared_error( trlab, tr_pred )
+
+mean_squared_error( tslab, ts_pred )
+root_mean_squared_error( tslab, ts_pred )
+
